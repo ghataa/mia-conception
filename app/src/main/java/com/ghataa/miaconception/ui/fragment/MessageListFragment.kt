d561fa3194adbe878b76ae019c95809ec87032d3
Gábor Horváth <dev.gaborhorvath@gmail.com>
2018-09-07 00:41:44 +0200

    Add MessageListFragment

---
+++ app/src/main/java/com/ghataa/miaconception/ui/fragment/MessageListFragment.kt
@@ -0,0 +1,19 @@
+package com.ghataa.miaconception.ui.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ghataa.miaconception.R
+
+class MessageListFragment : Fragment() {
+
+    companion object {
+        val TAG = MessageListFragment().javaClass.canonicalName
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_message_list, container, false)
+    }
+}
