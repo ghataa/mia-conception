b14e20f37698f6148c2c5a0b2a897537fa5efcd6
Gábor Horváth <dev.gaborhorvath@gmail.com>
2018-09-11 00:25:07 +0200

    Get random image for messaged

--- app/src/main/java/com/ghataa/miaconception/ui/adapter/MessageAdapter.kt
+++ app/src/main/java/com/ghataa/miaconception/ui/adapter/MessageAdapter.kt
@@ -1,12 +1,15 @@
 package com.ghataa.miaconception.ui.adapter
 
 import android.databinding.DataBindingUtil
+import android.net.Uri
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.ghataa.miaconception.R
 import com.ghataa.miaconception.databinding.ItemMessageBinding
 import com.ghataa.miaconception.model.Message
+import com.ghataa.miaconception.util.CommonUtil
+import com.squareup.picasso.Picasso
 
 class MessageAdapter(private val items: List<Message>) : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
 
@@ -22,6 +25,10 @@ class MessageAdapter(private val items: List<Message>) : RecyclerView.Adapter<Me
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding.message = items[position]
+
+        Picasso.get()
+                .load(Uri.parse("https://picsum.photos/400/?image=" + CommonUtil.getRandomImageNumber()))
+                .into(holder.binding.image)
     }
 
     override fun getItemCount() = items.size
