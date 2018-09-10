bd211b72962278e21d93d014f3853e7d69a27751
Gábor Horváth <dev.gaborhorvath@gmail.com>
2018-09-10 22:38:10 +0200

    Implement data binding

--- app/src/main/java/com/ghataa/miaconception/ui/fragment/MessageListFragment.kt
+++ app/src/main/java/com/ghataa/miaconception/ui/fragment/MessageListFragment.kt
@@ -1,19 +1,25 @@
 package com.ghataa.miaconception.ui.fragment
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ghataa.miaconception.R
+import com.ghataa.miaconception.databinding.FragmentMessageListBinding
 
 class MessageListFragment : Fragment() {
+
+    private lateinit var binding: FragmentMessageListBinding
 
     companion object {
         val TAG = MessageListFragment().javaClass.canonicalName
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_message_list, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_message_list, container, false)
+
+        return binding.root
     }
 }
