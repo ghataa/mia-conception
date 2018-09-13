64361d419d4875775a8b79b76c8785264b99a779
Gábor Horváth <dev.gaborhorvath@gmail.com>
2018-09-14 01:18:48 +0200

    Use data from firebase realtime database

--- app/src/main/java/com/ghataa/miaconception/util/CommonUtil.kt
+++ app/src/main/java/com/ghataa/miaconception/util/CommonUtil.kt
@@ -13,15 +13,15 @@ class CommonUtil {
         }
 
         fun getMessageList(): List<Message> {
-            val message1 = Message("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris neque tortor, porttitor ac posuere sed, dapibus nec tellus. Praesent aliquet elit consectetur a")
-            val message2 = Message("Aliquam non dignissim purus, quis interdum fel")
-            val message3 = Message("Ut non sollicitudin eros. Phasellus turpis nisi, laoreet et neque at, viverra dictum arcu.")
-            val message4 = Message("Nulla varius lacus sit amet ante vehicula, sed scelerisque nisi sollicitudin. Suspendisse potenti. In commodo enim et finibus posuere. Aliquam efficitur venenatis justo, vel condimentum sapien volutpat quis. Proin a augue mollis, consectetur erat vitae, lacinia nibh. Duis semper nulla urna, ut malesuada velit aliquet id. Nulla imperdiet luctus dignissim.")
-            val message5 = Message("Mauris odio nisi, commodo sed maximus nec, egestas non justo. Nunc vel purus molestie, pellentesque ante in, aliquam ligula. Aenean sed lacus fringilla, mollis nulla tristique, blandit est. Cras non volutpat metus, eget vulputate turpis. Pellentesque mollis fringilla velit, eu scelerisque eros fringilla a.")
-            val message6 = Message("nc, scelerisque ultrices accumsan sed, egestas eget risus. Cras lorem lorem, aliquet ut mattis vel, sagittis ut ipsum. Proin posuere, ligula in pretium posuere, en")
-            val message7 = Message("Ut non sollicitudin eros. Phasellus turpis nisi, laoreet et neque at, viverra dictum arcu.")
-            val message8 = Message("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris neque tortor, porttitor ac posuere sed, dapibus nec tellus. Praesent aliquet elit consectetur a")
-            val message9 = Message("Ut non sollicitudin eros. Phasellus turpis nisi, laoreet et neque at, viverra dictum arcu.")
+            val message1 = Message("Lorem ipsum", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris neque tortor, porttitor ac posuere sed, dapibus nec tellus. Praesent aliquet elit consectetur a")
+            val message2 = Message("Commodo sed maximus", "Aliquam non dignissim purus, quis interdum fel")
+            val message3 = Message("Ut non sollicitudin eros", "Ut non sollicitudin eros. Phasellus turpis nisi, laoreet et neque at, viverra dictum arcu.")
+            val message4 = Message("Lorem ipsum dolor sit amet", "Nulla varius lacus sit amet ante vehicula, sed scelerisque nisi sollicitudin. Suspendisse potenti. In commodo enim et finibus posuere. Aliquam efficitur venenatis justo, vel condimentum sapien volutpat quis. Proin a augue mollis, consectetur erat vitae, lacinia nibh. Duis semper nulla urna, ut malesuada velit aliquet id. Nulla imperdiet luctus dignissim.")
+            val message5 = Message("Ut non sollicitudin eros", "Mauris odio nisi, commodo sed maximus nec, egestas non justo. Nunc vel purus molestie, pellentesque ante in, aliquam ligula. Aenean sed lacus fringilla, mollis nulla tristique, blandit est. Cras non volutpat metus, eget vulputate turpis. Pellentesque mollis fringilla velit, eu scelerisque eros fringilla a.")
+            val message6 = Message("Lorem ipsum", "nc, scelerisque ultrices accumsan sed, egestas eget risus. Cras lorem lorem, aliquet ut mattis vel, sagittis ut ipsum. Proin posuere, ligula in pretium posuere, en")
+            val message7 = Message("Phasellus turpis", "Ut non sollicitudin eros. Phasellus turpis nisi, laoreet et neque at, viverra dictum arcu.")
+            val message8 = Message("Commodo sed maximus", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris neque tortor, porttitor ac posuere sed, dapibus nec tellus. Praesent aliquet elit consectetur a")
+            val message9 = Message("Nulla varius", "Ut non sollicitudin eros. Phasellus turpis nisi, laoreet et neque at, viverra dictum arcu.")
 
             return mutableListOf(message1, message2, message3, message4, message5, message6, message7, message8, message9)
         }
